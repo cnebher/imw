7f41f1edcd3dbb05440ed6fe7762672fd49b78df
cnebher <carlospddm@gmail.com>
2019-10-29 10:34:05 +0000

    Finalprogram

--- ut2/a1/main.py
+++ ut2/a1/main.py
@@ -1,13 +1,29 @@
-print ('¿Me das cambio por favor?')
+import sys
 
-billete50 = 50
-billete20 = 20
-billete10 = 10
-billete5  = 5
+money = int(sys.argv[1])
 
-moneda2   = 2
-moneda1   = 1
+print('Entrada:', money, 'Salida: ')
 
-dinero    =  input('¿Cuanto dinero quieres?')
+print()
 
-if dinero 
+if money >= 50:
+    bill50 = money//50
+    money = money%50
+    print('￮ Billete(s) de 50: ', bill50)
+if money >= 20:
+    bill20 = money//20
+    money = money%20
+    print('￮ Billete(s) de 20: ', bill20)
+if money >= 10:
+    bill10 = money//10
+    money = money%10
+    print('￮ Billete(s) de 10: ', bill10)
+if money >= 2:
+    coin2 = money//2
+    money = money%2
+    print('￮ moneda(s) de 2: ', coin2)
+if money >= 50:
+    coin1 = money//50
+    money = money%50
+    print('￮ modena(s) de 50: ', coin1)
+print()
