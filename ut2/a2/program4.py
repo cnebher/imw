ec3681683b90442705fa64d826806c0f889cc4f6
cnebher <carlospddm@gmail.com>
2019-11-03 20:16:13 +0000

    Change

---
+++ ut2/a2/program4.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+circumferenceR = float(sys.argv[1])
+
+print('Radio de entrada: r= ', circumferenceR)
+
+if 
+
+diameter  = 2 * circumferenceR
+
+perimeter = 2 * π * circumferenceR
+
+area      = π ** 2
