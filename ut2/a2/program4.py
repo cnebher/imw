38c64b412e1e48574319ac6df5b186be4aab6484
cnebher <carlospddm@gmail.com>
2019-11-05 08:52:46 +0000

    Program 4

--- ut2/a2/program4.py
+++ ut2/a2/program4.py
@@ -5,10 +5,17 @@ circumferenceR = float(sys.argv[1])
 
 print('Radio de entrada: r= ', circumferenceR)
 
-if 
 
 diameter  = 2 * circumferenceR
 
-perimeter = 2 * π * circumferenceR
+perimeter = 2 * math.pi * circumferenceR
 
-area      = π ** 2
+area      = math.pi * circumferenceR ** 2
+
+print('￮ Soluciones: ')
+
+print('￮ Diámetro: ', diameter)
+
+print('￮ Perímetro: ', perimeter)
+
+print('￮ Área: ', area)
