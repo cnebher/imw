a28483eab9965a6447433fee38447e148f30a365
cnebher <carlospddm@gmail.com>
2019-11-21 14:06:30 +0000

    Final programs

--- ut2/a4/program1.py
+++ ut2/a4/program1.py
@@ -1,10 +1,15 @@
 import sys
 import crayons
 
-num_dni = int(sys.argv[1])
+num_dni = sys.argv[1]
 
 letters = 'TRWAGMYFPDXBNJZSQVHLCKE'
 
-dniletter = num_dni % 23
-    
-print(f'{num_dni}{letters[dniletter]}')
+
+if len(num_dni) == 8:
+    num_dni = int(num_dni)
+    dniletter = num_dni % 23
+    letter = letters[dniletter]
+    print(f'{num_dni}{letter}')
+else:
+    print(crayons.red('DNI no valido'))
--- ut2/a4/program4.py
+++ ut2/a4/program4.py
@@ -1,7 +1,11 @@
 import sys
 
 numbers = sys.argv[1:]
-
 number = len(numbers)
+count_number = 0
 
-print(number)
+for seccion in numbers:
+    seccion = float(seccion)
+    count_number += seccion
+result = count_number / number
+print(f"La media de los valores es: {result}")
