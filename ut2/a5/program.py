4b8d0c5c95af231deb70c63245a804c56c7bed8f
cnebher <carlospddm@gmail.com>
2019-11-22 10:34:35 +0000

    change

---
+++ ut2/a5/program.py
@@ -0,0 +1,26 @@
+import sys
+
+# 1 numero vocales
+def num_vowels(text):
+    num_vowels = 0
+    for char in text.lower():
+        if char in 'aeiou':
+            num_vowels += 1
+    return num_vowels
+# 2 numero de espacios
+def num_whitespaces(text):
+    num_whitespaces = 0
+    for char in text.split():
+        if char in ' ':
+            num_whitespaces += 1
+    return num_whitespaces
+# 4 invertir palabras
+def reverse(text):
+    text = text[::-1]
+    return text
+
+if __name__ == '__main__':
+    text = sys.argv[1]
+    print('Number of vowels:', num_vowels(text))
+    print('Number of whitespaces:', num_whitespaces(text))
+    print('Reverse of text:', reverse(text))
