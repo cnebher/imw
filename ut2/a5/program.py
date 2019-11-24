9278e922c942a53eecedcc3cb39a5ba4a609a7bd
cnebher <carlospddm@gmail.com>
2019-11-24 21:27:50 +0000

    change

--- ut2/a5/program.py
+++ ut2/a5/program.py
@@ -16,15 +16,51 @@ def num_whitespaces(text):
     return num_whitespaces
 # 3 numero de digitos
 def num_digits(text):
+    num_digits = 0
+    for char in text:
+        if char.isdigit():
+            num_digits += 1
+    return num_digits
+# 4 numero de palabras
+def num_words(text):
+    text_list = text.split()
+    num_words = len(text_list)
 
-
-# 4 invertir palabras
+    return num_words
+# 5 invertir palabras
 def reverse(text):
     text = text[::-1]
     return text
-
+# 6 longitud
+def length(text):
+    leng = len(text)
+    return leng
+# 7 mitad
+# 8 vocales mayus
+def upper_vowels(text):
+    vocals = ""
+    for char in text:
+        if char.upper() in "AEIOU":
+            vocals += char.upper()
+        else:
+            vocals += char
+    return vocals
+# 9 palabras ordenadas
+def sorted_by_words(text):
+    text_list = text.split()
+    text = sorted(text_list)
+    sorted_by_words = " ".join(text)
+    return sorted_by_words
+# 10 longitud de palabras
 if __name__ == '__main__':
     text = sys.argv[1]
     print('Number of vowels:', num_vowels(text))
     print('Number of whitespaces:', num_whitespaces(text))
+    print('Number of digits:', num_digits(text))
+    print('Number of words:', num_words(text))
     print('Reverse of text:', reverse(text))
+    print('Length of text:', length(text))
+    #print('Halfs of text:', halfs(text))
+    print('Text with uppercased vowels:', upper_vowels(text))
+    print('Sorted by words:', sorted_by_words(text))
+    #print('Length of words:', length_of_words(text))
