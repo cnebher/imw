35128dcccfb7521fd4967ff9803718561f33037c
cnebher <carlospddm@gmail.com>
2019-11-26 11:33:08 +0000

    final program

--- ut2/a5/program.py
+++ ut2/a5/program.py
@@ -1,42 +1,48 @@
 import sys
 
-# 1 numero vocales
 def num_vowels(text):
     num_vowels = 0
-    for char in text.lower():
-        if char in 'aeiou':
+    for words in text.lower():
+        if words in 'aeiou':
             num_vowels += 1
     return num_vowels
-# 2 numero de espacios
+
 def num_whitespaces(text):
     num_whitespaces = 0
-    for char in text:
-        if char == ' ':
+    for space in text:
+        if space == ' ':
             num_whitespaces += 1
     return num_whitespaces
-# 3 numero de digitos
+
 def num_digits(text):
     num_digits = 0
     for char in text:
         if char.isdigit():
             num_digits += 1
     return num_digits
-# 4 numero de palabras
+
 def num_words(text):
     text_list = text.split()
     num_words = len(text_list)
 
     return num_words
-# 5 invertir palabras
+
 def reverse(text):
     text = text[::-1]
     return text
-# 6 longitud
+
 def length(text):
-    leng = len(text)
-    return leng
-# 7 mitad
-# 8 vocales mayus
+    length = len(text)
+    return length
+
+def halfs(text):
+    text_lenght = len(text)
+    if text_lenght >= 2:
+         mid = text_lenght // 2
+         first = text[:mid]
+         second = text[mid:]
+    return ' | '.join([first, second])
+
 def upper_vowels(text):
     vocals = ""
     for char in text:
@@ -45,13 +51,20 @@ def upper_vowels(text):
         else:
             vocals += char
     return vocals
-# 9 palabras ordenadas
+
 def sorted_by_words(text):
     text_list = text.split()
     text = sorted(text_list)
     sorted_by_words = " ".join(text)
     return sorted_by_words
-# 10 longitud de palabras
+
+def length_of_words(text):
+    mylist = []
+    text_lenght = text.split()
+    for word in text_lenght:
+        mylist.append(str(len(word)))
+    text_string = ' '.join(mylist)
+    return text_string
 if __name__ == '__main__':
     text = sys.argv[1]
     print('Number of vowels:', num_vowels(text))
@@ -60,7 +73,7 @@ if __name__ == '__main__':
     print('Number of words:', num_words(text))
     print('Reverse of text:', reverse(text))
     print('Length of text:', length(text))
-    #print('Halfs of text:', halfs(text))
+    print('Halfs of text:', halfs(text))
     print('Text with uppercased vowels:', upper_vowels(text))
     print('Sorted by words:', sorted_by_words(text))
-    #print('Length of words:', length_of_words(text))
+    print('Length of words:', length_of_words(text))
